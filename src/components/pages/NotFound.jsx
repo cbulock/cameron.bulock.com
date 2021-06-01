160cf40bf2d580913bee8be3fd65d2b796cebb17
Cameron Bulock <cameron@bulock.com>
2021-06-01 19:08:17 -0400

    update 404 page

--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+
+const STypography = styled(Typography)`
+	color: ${({ theme }) => theme.palette.text.primary};
+`;
 
 export default () => {
-	return <p>Page Not Found</p>;
+	return (
+		<Box m={2}>
+			<STypography>Page not found</STypography>
+		</Box>
+	);
 };
